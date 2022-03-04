4e398bda468e683177d392a170b13f6a705a5893
Linh Nguyen <lnguyen1148@csu.fullerton.edu>
2022-03-04 13:04:19 -0800

    First commit

---
+++ basic_dash_app.py
@@ -0,0 +1,27 @@
+import dash
+from dash import html
+from dash import dcc
+import plotly.express as px
+import pandas as pd
+
+# Read in data
+data = pd.read_csv("ukr_asylum.csv", usecols=["Year", "Asylum-seekers"])
+
+# Create a plotly figure for dcc.Graph()
+fig = px.line(data, x="Year", y="Asylum-seekers", title="Asylum seekers from Ukraine 2016-2021")
+
+app = dash.Dash(__name__)
+app.title = "Asylum seekers from Ukraine 2016-2021"
+
+app.layout = html.Div(
+    id="app-container",
+    children=[
+        html.H1("Asylum seekers from Ukraine 2016-2021"),
+        html.P("Data extracted from the UN Refugee Agency"),
+        dcc.Graph(figure=fig)
+    ]
+)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
